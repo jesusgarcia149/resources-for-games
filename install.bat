d9d2b620795e945699384d472d771fafa1cb6300
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-08-01 23:53:53 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,14 @@
+powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
+
+choco install dotnet4.7.2 -y
+choco install directx -y
+choco install jre8 -y
+choco install vcredist2005 -y
+choco install vcredist2008 -y
+choco install vcredist2010 -y
+choco install vcredist2012 -y
+choco install vcredist2013 -y
+choco install vcredist2015 -y
+choco install vcredist2017 -y
+choco install vcredist2019 -y
