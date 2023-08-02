cd7669b3aba63565c67efd3d469341d3cfbc5859
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-08-01 23:58:20 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,8 @@
 powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
 
+choco install steam -y
+
 choco install msiafterburner -y
 
 choco install dotnet4.7.2 -y
