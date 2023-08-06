58a373f95aab83e19be942ca2e0ad17176a7a0da
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-08-06 16:38:48 +0200

    Add files via upload

--- gamemode/gamemode - exit.bat
+++ gamemode/gamemode - exit.bat
@@ -1,5 +1,6 @@
-taskkill /IM Heroic.exe /F
 taskkill /IM MSIAfterburner.exe /F
 taskkill /IM RTSS.exe /F
+taskkill /IM RTSSHooksLoader64.exe /F
 taskkill /IM HandheldCompanion.exe /F
 taskkill /IM MEGAsync.exe /F
+taskkill /IM Heroic.exe /F
