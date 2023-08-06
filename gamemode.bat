76b4134b9a64220313b2bd2ebc390a57863e5ac5
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-08-06 06:51:20 +0200

    Create gamemode.bat

---
+++ gamemode.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\Administrador1\AppData\Local\Programs\heroic"
+start Heroic.exe
+cd "C:\Program Files (x86)\MSI Afterburner\"
+start MSIAfterburner.exe
+cd "C:\Program Files\Handheld Companion\"
+start HandheldCompanion.exe
+cd "C:\ProgramData\MEGAsync"
+start MEGAsync.exe
