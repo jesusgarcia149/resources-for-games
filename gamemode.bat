047902c0c2ad4925a8ed3bb8fb6dcade1fe2cd2a
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-08-06 16:25:43 +0200

    Delete gamemode.bat

--- gamemode.bat
+++
@@ -1,8 +0,0 @@
-cd "C:\Users\Administrador1\AppData\Local\Programs\heroic"
-start Heroic.exe
-cd "C:\Program Files (x86)\MSI Afterburner\"
-start MSIAfterburner.exe
-cd "C:\Program Files\Handheld Companion\"
-start HandheldCompanion.exe
-cd "C:\ProgramData\MEGAsync"
-start MEGAsync.exe
