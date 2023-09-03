5b23280303d8299fd5230d9b86847b702cd33c6c
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-09-03 10:15:17 -0400

    Add files via upload

--- gamemode/gamemode - start.bat
+++ gamemode/gamemode - start.bat
@@ -1,8 +1,9 @@
-cd "C:\Users\Administrador1\AppData\Local\Programs\heroic"
+cd "C:\Users\Gamemode\AppData\Local\Programs\heroic"
 start Heroic.exe
-cd "C:\Program Files (x86)\MSI Afterburner\"
+timeout 10
+cd "C:\Program Files (x86)\MSI Afterburner"
 start MSIAfterburner.exe
-cd "C:\Program Files\Handheld Companion\"
+cd "C:\Program Files\Handheld Companion"
 start HandheldCompanion.exe
-cd "C:\ProgramData\MEGAsync"
+cd "C:\Users\Gamemode\AppData\Local\MEGAsync"
 start MEGAsync.exe
