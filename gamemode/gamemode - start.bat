6b324c2937cd41810bb74b55463f020741a9e9c7
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-09-09 13:49:13 -0400

    Update gamemode - start.bat

--- gamemode/gamemode - start.bat
+++ gamemode/gamemode - start.bat
@@ -1,9 +1,9 @@
-cd "C:\Users\Gamemode\AppData\Local\Programs\heroic"
+cd "C:\Users\%USERNAME%\AppData\Local\Programs\heroic"
 start Heroic.exe
 timeout 10
 cd "C:\Program Files (x86)\MSI Afterburner"
 start MSIAfterburner.exe
 cd "C:\Program Files\Handheld Companion"
 start HandheldCompanion.exe
-cd "C:\Users\Gamemode\AppData\Local\MEGAsync"
+cd "C:\Users\%USERNAME%\AppData\Local\MEGAsync"
 start MEGAsync.exe
