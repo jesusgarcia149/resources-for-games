5bbbf99f76457c82c9fab44146da9d6853736bd3
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-09-22 18:32:10 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,37 +1,37 @@
 sudo apt update
 
 sudo apt install steam -y
-# choco install epicgameslauncher -y
-# choco install uplay -y
-# choco install battlenet -y
-# choco install bethesdanet -y
-# choco install goggalaxy -y
-# choco install origin -y
+# sudo apt install epicgameslauncher -y #not exist
+# sudo apt install uplay -y #not exist
+# sudo apt install battlenet -y #not exist 
+# sudo apt install bethesdanet -y #not exist
+# sudo apt install goggalaxy -y #not exist
+# sudo apt install -y #not exist
 
-# choco install nvidia-geforce-now -y
-# choco install retroarch -y #not found
-# choco install rockstar-launcher -y
-# choco install heroicgameslauncher -y #not exist
+# sudo apt install nvidia-geforce-now -y #not exist
+# sudo apt install retroarch -y
+# sudo apt install rockstar-launcher -y #not exist
+# sudo apt install heroicgameslauncher -y #not exist
 
-# choco install msiafterburner -y
+# choco install msiafterburner -y #not exist
 # choco install handheldcompanion -y #not exist
 # choco install optimizer -y #not exist
-# choco install gamesavemanager -y
-# choco install megasync -y
+# choco install gamesavemanager -y #not exist
+# choco install megasync -y #not exist
 
 # choco install 3dp-chip -y #not exist
 
-# choco install dotnet4.7.2 -y
-# choco install directx -y
-# choco install jre8 -y
-# choco install vcredist2005 -y
-# choco install vcredist2008 -y
-# choco install vcredist2010 -y
-# choco install vcredist2012 -y
-# choco install vcredist2013 -y
-# choco install vcredist2015 -y
-# choco install vcredist2017 -y
-# choco install vcredist2019 -y
+# choco install dotnet4.7.2 -y #not exist
+# choco install directx -y #not exist
+# choco install jre8 -y #not exist
+# choco install vcredist2005 -y #not exist 
+# choco install vcredist2008 -y #not exist
+# choco install vcredist2010 -y #not exist 
+# choco install vcredist2012 -y #not exist
+# choco install vcredist2013 -y #not exist 
+# choco install vcredist2015 -y #not exist 
+# choco install vcredist2017 -y #not exist 
+# choco install vcredist2019 -y #not exist
 
 sudo apt install acetoneiso -y
 sudo apt install 7zip -y
@@ -40,10 +40,3 @@ sudo apt install qbittorrent -y
 
 sudo apt install wget -y
 
-# wget https://www.3dpchip.com/new/3DP_Chip_v2307.exe
-# wget https://buildbot.libretro.com/stable/1.15.0/windows/x86_64/RetroArch-Win64-setup.exe
-# wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.9.1/Heroic-2.9.1-Setup-x64.exe
-# wget https://github.com/Valkirie/HandheldCompanion/releases/download/0.16.2.5/HandheldCompanion-0.16.2.5.exe
-# wget https://github.com/hellzerg/optimizer/releases/download/15.6/Optimizer-15.6.exe
-# return Calculator = Optimizer > Apps UWP > Microsoft.WindowsCalculator
-# wget https://download.urban-vpn.com/latest/UrbanVPN2.exe
